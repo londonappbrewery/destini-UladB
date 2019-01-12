19965a1a23e841f38fed0f723a6fba05e8c99285
UladB <UladB@users.noreply.github.com>
2019-01-12 21:25:59 +0000

    Submission 4

---
+++ Destini/Story.swift
@@ -0,0 +1,25 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Uladzimir Bialko on 11/01/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Story {
+
+    let storyText : String
+    let storyAnswer1 : String
+    let storyAnswer2 : String
+
+    init(text : String, answer1 : String, answer2 : String) {
+
+    storyText = text
+    storyAnswer1 = answer1
+    storyAnswer2 = answer2
+        
+    }
+
+}
--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -2,45 +2,32 @@
 //  ViewController.swift
 //  Destini
 //
+//  Created by Philipp Muellauer on 01/09/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
 
 import UIKit
+import Darwin
 
 class ViewController: UIViewController {
-
-    // Our strings
-    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let answer1a = "Better ask him if he\'s a murderer first."
-    let answer1b = "I\'ll hop in. Thanks for the help!"
-    
-    let story2 = "He nods slowly, unphased by the question."
-    let answer2a = "At least he\'s honest. I\'ll climb in."
-    let answer2b = "Wait, I know how to change a tire."
-    
-    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let answer3a = "I love Elton John! Hand him the cassette tape."
-    let answer3b = "It\'s him or me! You take the knife and stab him."
-    
-    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
+    let allStories = StoryBank()
+    
     // TODO Step 5: Initialise instance variables here
-    var storyIndex : Int = 1
-    var firstFork : Bool = false //indicates if user progressed to story 2
-    var secondFork: Bool = false //indicates if user progressed to story 3 (final fork)
-    var endStoryFlag: Bool = false
+    var storyIndex = 1
+    var branch : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
-        checkStoryIndex()
+    findStory(forStoryIndex: storyIndex)
         
     }
 
@@ -48,85 +35,99 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
-        // TODO Step 6: Modify the IF-Statement to complete the story
         if sender.tag == 1 {
-           storyIndex += 1
-        }
-        else if sender.tag == 2 {
-            storyIndex += 2
-        }
 
-        checkStoryIndex()
+            storyIndex = storyIndex + 2
+            
+        }
         
-        storyIndex = 1
+        else if sender.tag == 2 {
+            
+            storyIndex = storyIndex + 1
+            
+        }
+                
+        // TODO Step 6: Modify the IF-Statement to complete the story
+        print(storyIndex)
+        print(branch)
         
-        //restart the story if it's over
-        if endStoryFlag == true {
-            
-            let alert = UIAlertController (title: "End of the story", message: "Do you want to start over?", preferredStyle: .actionSheet)
-            
-            let restartAction = UIAlertAction(title: "Restart!", style: .default, handler: { (UIAlertAction) in
-                self.startOver()
-            })
-            alert.addAction(restartAction)
-            
-            present(alert, animated: true, completion: nil)
+        findStory(forStoryIndex: storyIndex)
+    }
+    
+    
+    //figure out how to add if statement here
+    //or a separate function how to guide through the story, try to make as generic as possible
+    func findStory(forStoryIndex : Int) {
+        
+        if storyIndex == 1 && branch == "" {
+            display(storyNumber: 1)
+        }
+        else if storyIndex == 3 && branch == "" {
+            display(storyNumber: 3)
+            branch = "a"
+        }
+        else if storyIndex == 2 && branch == "" {
+            display(storyNumber: 2)
+            branch = "b"
+        }
+        else if storyIndex == 5 && branch == "a" {
+            display(storyNumber: 6)
+            hideButtonsReset()
+        }
+        else if storyIndex == 4 && branch == "a" {
+            display(storyNumber: 5)
+            hideButtonsReset()
+        }
+        else if storyIndex == 3 && branch == "b" {
+            display(storyNumber: 4)
+            hideButtonsReset()
+        }
+        else if storyIndex == 4 && branch == "b" {
+            display(storyNumber: 3)
+        }
+        else if storyIndex == 6 && branch == "b" {
+            display(storyNumber: 6)
+            hideButtonsReset()
+        }
+        else if storyIndex == 5 && branch == "b" {
+            display(storyNumber: 5)
+            hideButtonsReset()
         }
     }
     
-    func checkStoryIndex() {
+    func display(storyNumber : Int) {
         
-        if storyIndex == 1 {
-            storyTextView.text = story1
-            topButton.setTitle(answer1a, for: .normal)
-            bottomButton.setTitle(answer1b, for: .normal)
-        }
-        else if storyIndex == 2 && firstFork == false && secondFork == false {storyTextView.text = story2
-            topButton.setTitle(answer2a, for: .normal)
-            bottomButton.setTitle(answer2b, for: .normal)
-            firstFork = true
-        }
-        else if storyIndex == 3 && firstFork == false && secondFork == false || (storyIndex == 2 && firstFork == true && secondFork == false)  {storyTextView.text = story3
-            topButton.setTitle(answer3a, for: .normal)
-            bottomButton.setTitle(answer3b, for: .normal)
-            secondFork = true
-        }
-        else if storyIndex == 3 && firstFork == true && secondFork == false {
-            storyTextView.text = story4
-            endStory()
-        }
-        else if storyIndex == 3 && secondFork == true {
-            storyTextView.text = story5
-            endStory()
-        }
-        else if storyIndex == 2 && secondFork == true {
-            storyTextView.text = story6
-            endStory()
-        }
+        storyTextView.text = allStories.list[storyNumber].storyText
+        
+        topButton.setTitle(allStories.list[storyNumber].storyAnswer1, for: .normal)
+        bottomButton.setTitle(allStories.list[storyNumber].storyAnswer2, for: .normal)
         
     }
-    
-    func endStory() {
+    func hideButtonsReset(){
         topButton.isHidden = true
         bottomButton.isHidden = true
-        endStoryFlag = true
+        let alert = UIAlertController(title: "The End", message: "Want to do it again?", preferredStyle: .actionSheet)
+        let restartGame = UIAlertAction(title: "Start Again", style: .default) { (UIAlertAction) in
+            self.startOver()
+        }
+        let endGame = UIAlertAction(title: "Exit", style: .default) { (UIAlertAction) in
+            exit(0)
+        }
+        alert.addAction(restartGame)
+        alert.addAction(endGame)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+            self.present(alert,animated: true)
+        })
+        
     }
     
-    func startOver() {
+    func startOver(){
+        
         storyIndex = 1
-        firstFork = false
-        secondFork = false
-        endStoryFlag = false
+        branch = ""
+        findStory(forStoryIndex: storyIndex)
         topButton.isHidden = false
         bottomButton.isHidden = false
-        checkStoryIndex()
     }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
